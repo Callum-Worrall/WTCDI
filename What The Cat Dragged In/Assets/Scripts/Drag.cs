287447d986add09d2b4321f7975117d57a580f60
callum worrall <callum.worrall71@gmail.com>
2015-03-25 16:40:24 +1100

    First Project GO!

---
+++ What The Cat Dragged In/Assets/Scripts/Drag.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag : MonoBehaviour
+{
+
+	public bool pickedUp;
+
+	// Use this for initialization
+	void Start ()
+	{
+		pickedUp = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//if(pickedUp == true)
+		//{
+		//	transform.GetChild(0).position = new Vector3 (0, 0, 0);
+		//	transform.GetChild(0).position = new Vector3 (0, 0, 0);
+		//	transform.GetChild(0).position = new Vector3 (0, 0, 0);
+		//}
+
+		if(Input.GetButtonDown("Pickup"))
+		{
+			if (pickedUp == false)
+			{
+				PickUp();
+				//transform.GetChild(0).rotation = new Quaternion.Euler;
+				//transform.GetChild(0).localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			}
+			else
+				Drop();
+		}
+	}
+
+	void PickUp()
+	{
+		GameObject[] Corpse = GameObject.FindGameObjectsWithTag("Corpse");
+
+		for (int i = 0; i < Corpse.Length; i++)
+		{
+			if (Corpse[i].transform.position.x < (transform.position.x + 1.25f) &&
+			    Corpse[i].transform.position.x > (transform.position.x - 1.25f) && pickedUp == false)
+			{
+				//Corpse[i].transform.position = new Vector3 (0, 0, 0);
+
+				Corpse[i].transform.parent = transform;
+
+
+
+				pickedUp = true;
+			}
+		}
+	}
+
+	void Drop()
+	{
+		//transform.GetChild() = 
+
+		transform.GetChild(0).parent = null;
+		
+		//Child.transform;
+
+		pickedUp = false;
+	}
+}
---
+++ What The Cat Dragged In/Assets/Scripts/Player.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+	//////////VARIABLES///////////
+	#region
+	
+	// Utility //
+	float timer = 0f;
+	
+	// Player Stats //
+	public int currentHealth = 10;
+	public int maxHealth = 10;
+	
+	// Speed Settings //
+	public Vector3 speed;
+	public Vector3 sprintSpeed;
+	public Vector3 jumpSpeed;
+	public Vector3 gravity;
+	
+	// Sprinting Variables //
+	public float sprintTime = 1.0f;
+	public bool sprinting = false;
+	float sprintingTimer;
+	
+	// Jumping Variables //
+	public float jumpHeight = 2.0f;
+	public float maxJumpHeight = 2.0f;
+	float maxJumpSpeed;
+	public bool grounded = true;
+	public bool reachedApex;
+	float jumpVelocity = 0.0f;
+	Vector3 jumpVector;
+	public float jumpCounter;
+	
+	public List<GameObject> Children;
+	
+	
+	#endregion
+	/////////////////////////////
+	
+	//////////FUNCTIONS///////////
+	
+	// Initialization //
+	void Start ()
+	{
+
+	}
+	
+	// Update //
+	void Update ()
+	{
+		timer += Time.deltaTime;
+	
+
+		InputManager();
+	}
+
+	void InputManager()
+	{
+		Movement();
+		Sprint();
+		Jump();
+	}
+
+	// Movement //
+	void Movement()
+	{
+		//if(Input.GetButtonDown("Horizontal"))
+		//{
+			if(Input.GetAxis("Horizontal") < 0)
+			{
+				transform.position -= speed * Time.deltaTime;
+			}
+		else if(Input.GetAxis("Horizontal") > 0)
+				transform.position += speed * Time.deltaTime;
+		//}
+	}
+
+
+	// Sprint //
+	void Sprint()
+	{
+		if (Input.GetButtonDown("Sprint") &&
+		    sprinting == false &&
+		    grounded == true)
+		{
+			sprinting = true;
+			sprintingTimer = 0.0f;
+			Debug.Log("Sprinting!", gameObject);
+		}
+		
+		if(sprinting == true)
+		{
+			sprintingTimer += Time.deltaTime;
+		}
+		
+		if (sprintingTimer > sprintTime && grounded == true)
+		{
+			sprinting = false;
+		}
+	}
+	
+	// Jump //
+	void Jump()
+	{
+		//Check if grounded
+		if(Input.GetButtonDown("Jump") && grounded == true)
+		{
+			grounded = false;
+			jumpVelocity = 0.0f;
+		}
+		
+		//Check if apex is reached and not on the ground, if true continue to jump
+		if (grounded == false && reachedApex == false)
+		{
+			float newPosition = Mathf.SmoothDamp(
+				transform.position.y,
+				jumpHeight, 
+				ref jumpVelocity, 
+				1.0f * Time.deltaTime, 
+				jumpSpeed.y);
+			
+			transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+		}
+		
+		//Check if jump height is reached
+		if (transform.position.y >= maxJumpHeight)
+		{
+			reachedApex = true;
+			jumpVelocity = 0.0f;
+		}
+		
+		//Start acting gravity on the player to bring it back to the ground
+		if (reachedApex == true)
+		{
+			float newPosition = Mathf.SmoothDamp(
+				transform.position.y,
+				0.0f,
+				ref jumpVelocity,
+				1.0f * Time.deltaTime, 
+				gravity.y);
+			
+			transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+
+		}
+		
+		//Player land
+		if (grounded == false && transform.position.y <= 0.0f)
+		{
+			transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+			grounded = true;
+			reachedApex = false;
+		}        
+	}
+	
+	
+	bool GetSprinting()
+	{
+		return sprinting;
+	}
+
+	void AdjustCurrentHealth(int health)
+	{
+		currentHealth += health;
+	}
+}
